165d7c1ec6baefeb0be0209c96860146cb78fecd
luisEfe80 <luismendez80@hotmail.com>
2021-06-19 00:38:57 -0400

    content adjustment

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -65,7 +65,59 @@
             .p-4
               p El segundo pilar es <b>la reproducibilidad,</b> es decir, que toda proposición científica tiene que ser susceptible de ser falsada o refutada; esto significa que se podrían obtener resultados diferentes a los hechos a través de hipótesis.
     p.mb-5  El método científico usa métodos definitorios, métodos clasificatorios, métodos estadísticos, métodos hipotético-deductivos, procedimientos de medición, etc. Según esto, referirse al método científico es referirse a este conjunto de tácticas empleadas para constituir el conocimiento, sujetas al devenir histórico, y que eventualmente podrían ser otras en el futuro. A continuación, se presenta una clasificación o tipología de algunos métodos avalados en la comunidad científica según Labajo (s.f.):
-    //-- Aqui vienen los tabs
+    
+    //--TABS
+    TabsB.color-acento-contenido.mb-5
+      .py-4.py-md-5(titulo="Método empírico-analítico" :icono="require('@/assets/curso/t2-ic4.svg')")
+        .row.justify-content-center
+          .col-md-6.mb-4.mb-md-0.py-5
+            p.mb-5 El conocimiento se presenta de manera lógica, autocorrectiva y progresiva. Es característico de las ciencias naturales y sociales o humanas y, a su vez caracteriza a las ciencias descriptivas. Es el método general más utilizado. Se basa en la lógica empírica. Dentro de este se pueden observar varios métodos específicos con técnicas particulares.        
+          .col-md-4
+            figure
+              img.h210(src='@/assets/curso/t2-ic4.svg', alt='imagen decorativa')
+              
+      .py-4.py-md-5(titulo="Método hermenéutico" :icono="require('@/assets/curso/t2-ic5.svg')")
+        .row.justify-content-center
+          .col-md-6.mb-4.mb-md-0.py-5
+            p.mb-5 Es el estudio de la coherencia interna de los textos, la filología, la exégesis de libros sagrados y el estudio de la coherencia de las normas y principios.     
+          .col-md-4
+            figure
+              img.h210(src='@/assets/curso/t2-ixx1.svg', alt='imagen decorativa')
+        
+      .py-4.py-md-5(titulo="Método dialéctico" :icono="require('@/assets/curso/t2-ic6.svg')")
+        .row.justify-content-center
+          .col-md-6.mb-4.mb-md-0.py-5
+            p.mb-5 Considera los fenómenos históricos y sociales en continuo movimiento. Dio origen al materialismo histórico.     
+          .col-md-4
+            figure
+              img.h210(src='@/assets/curso/t2-ixx3.svg', alt='imagen decorativa')
+
+      .py-4.py-md-5(titulo="Método histórico" :icono="require('@/assets/curso/t2-ic7.svg')")
+        .row.justify-content-center
+          .col-md-6.mb-4.mb-md-0.py-5
+            p.mb-5 Está vinculado al conocimiento de las distintas etapas de los objetos en su sucesión cronológica. Mediante el método histórico se analiza la trayectoria concreta de una teoría, y su condicionamiento a los diferentes períodos de la historia.     
+          .col-md-4
+            figure
+              img.h210(src='@/assets/curso/t2-ixx4.svg', alt='imagen decorativa')
+        
+      
+      .py-4.py-md-5(titulo="Método sistémico" :icono="require('@/assets/curso/t2-ic8.svg')")
+        .row.justify-content-center
+          .col-md-6.mb-4.mb-md-0.py-5
+            p.mb-5 Está dirigido a modelar el objeto mediante la determinación de sus componentes, así como las relaciones entre ellos. Esas relaciones determinan por un lado la estructura del objeto y, por otro, su dinámica.  
+          .col-md-4
+            figure
+              img.h210(src='@/assets/curso/t2-ixx5.svg', alt='imagen decorativa')
+
+      
+      .py-4.py-md-5(titulo="Método lógico" :icono="require('@/assets/curso/t2-ic9.svg')")
+        .row.justify-content-center
+          .col-md-6.mb-4.mb-md-0.py-5
+            p.mb-5 Es otra gran rama del método científico, aunque es más clásica y de menor fiabilidad. Su unión con el método empírico dio lugar al método hipotético-deductivo, uno de los más fiables hoy en día.
+          .col-md-4
+            figure
+              img.h210(src='@/assets/curso/t2-ixx6.svg', alt='imagen decorativa')
+
 
 
 
@@ -117,13 +169,6 @@
           .tarjeta.tarjeta--gris
             .p-4
               p <b>La instrucción y tecnología</b> hacen referencia a los conocimientos que debe tener un aprendiz desde educación preescolar hasta el grado once, que le permitan obtener y desarrollar habilidades para una sociedad eminentemente tecnológica.
-    .row.mb-5
-      .col-lg-8
-        p.mb-3  En los Estados Unidos a principios del siglo XXI se comenzaron a desarrollar las actividades de instrucción en ingeniería, y para ello se recurrió al proceso de diseño. La forma de lanzar una actividad de instrucción en ingeniería siguió los pasos de un proceso minucioso y metódico, con esto se apoyó de manera seria y profunda la instrucción en esta disciplina. La aplicación del proceso de diseño ingeniería es la entrada disimulada a la introducción ingeniería en el ámbito escolar.
-        p.mb-3  Los estándares de ciencias NGSS (Next Generation Science Standards) presentan las prácticas de ciencias e ingeniería como parte fundamental, recordemos que la estructura de los NGSS se basa en las tres dimensiones: a) las prácticas de ciencias en ingeniería, b) las ideas fundamentales disciplinares y c) los conceptos transversales. La ingeniería se encuentra en la dimensión de las prácticas de ciencias y también como cuarto grupo de las ideas fundamentales disciplinares.
-      .col-lg-4
-        figure.mb-4
-          img(src='@/assets/curso/t2-i10.png', alt='Profesor explicando a sus estudiantes frente al computador')
 
 
 
@@ -132,12 +177,128 @@
     Separador
     #t_2_3.titulo-segundo
       .h4 2.3 Influencia de la ingeniería en el contexto STEM
+    .row.mb-5
+      .col-lg-8
+        p.mb-3  En los Estados Unidos a principios del siglo XXI se comenzaron a desarrollar las actividades de instrucción en ingeniería, y para ello se recurrió al proceso de diseño. La forma de lanzar una actividad de instrucción en ingeniería siguió los pasos de un proceso minucioso y metódico, con esto se apoyó de manera seria y profunda la instrucción en esta disciplina. La aplicación del proceso de diseño ingeniería es la entrada disimulada a la introducción ingeniería en el ámbito escolar.
+        p.mb-3  Los estándares de ciencias NGSS (Next Generation Science Standards) presentan las prácticas de ciencias e ingeniería como parte fundamental, recordemos que la estructura de los NGSS se basa en las tres dimensiones: a) las prácticas de ciencias en ingeniería, b) las ideas fundamentales disciplinares y c) los conceptos transversales. La ingeniería se encuentra en la dimensión de las prácticas de ciencias y también como cuarto grupo de las ideas fundamentales disciplinares.
+      .col-lg-4
+        figure.mb-4
+          img(src='@/assets/curso/t2-i10.png', alt='Profesor explicando a sus estudiantes frente al computador')
+    .row.justify-content-center
+      .col-lg-10
+        .cajon.color-secundario.p-4.mb-5
+          p.mb-3  La educación STEM busca que los aprendices indaguen sobre los problemas importantes y urgentes del mundo, y para ello se requiere que no solo se aborden desde la perspectiva de las ciencias, sino de la ingeniería. La ciencia busca responder preguntas para entender el mundo natural, pero la ingeniería busca resolver los problemas y encontrar soluciones efectivas para desarrollar, inventar e innovar en el mundo construido por el hombre (Purzer, Strobel y Cardella, 2014).
+    p.mb-5  Por lo mencionado hasta aquí, hablar de ingeniería remite al diseño, el cual es necesario pensarlo desde unos criterios o restricciones que intervienen en el mismo. A continuación, se destacan algunos para poder ser identificados:
+    figure.mb-4
+      img(src='@/assets/curso/t2-ix1.svg', alt='infografía de restricciones')
+    .row.mb-5
+      .col-lg-8
+        .cajon.color-secundario.p-4
+          p.mb-3  Luego de percibir los criterios que intervienen en el diseño, es necesario presentar las fases del pensamiento de diseño en ingeniería en el contexto de la enseñanza de <b>la educación STEM</b> para los futuros ciudadanos del siglo XXI. Se puede afirmar que todo lo que rodea al entorno social y natural enfrenta diariamente cambios y esto conlleva al surgimiento de nuevos problemas en diferentes temáticas, para el caso de la ingeniería uno de los aspectos que manejan los profesionales es poder conocer la influencia de los diferentes parámetros o variables de manera previa, que les permita solucionar un problema con restricciones como límite de costos, tiempo de ejecución e implementación, eficiencia, capacidad, entre otras. A continuación, se presentan las fases con una caracterización que se ha compilado para que usted reconozca el paso a paso del pensamiento de diseño desde el Design Thinking. <b>Adelante con la exploración.</b>
+      .col-lg-4
+        figure.mb-4
+          img(src='@/assets/curso/t2-i11.png', alt='niños sentados en una mesa explorando prototipos de robots')
+    .row.mb-5.justify-content-center
+      .col-lg-10
+        figure.mb-4
+          img(src='@/assets/curso/t2-i12.svg', alt='Fases del design thinking')
+
+    //--TABS
+    TabsA.color-acento-botones.mb-5
+      //- .tarjeta debe ir acompañado de una de una de estas clases => 
+      //- .color-primario, .color-secundario, .color-acento-contenido, .color-acento-botones
+      //- estas clases tambien tienen un modificador --borde
+      .tarjeta.tarjeta--gris.p-4(titulo="Fase 1: Empatizar")
+        .row
+          .col-lg-3
+            figure
+              img.h150(src='@/assets/curso/t2-ic-x5.svg', alt='icono decorativo')
+          .col-lg-9
+            p.mb-3  El proceso de pensamiento de diseño comienza con una profunda comprensión de las necesidades de los aprendices implicados en la solución que estemos desarrollando, y también de su entorno. <b>Hay que ser capaces de ponerse en la piel del equipo para generar soluciones consecuentes con sus realidades. </b>
+      
+      //- .tarjeta debe ir acompañado de una de una de estas clases => 
+      //- .color-primario, .color-secundario, .color-acento-contenido, .color-acento-botones
+      //- estas clases tambien tienen un modificador --borde
+      .tarjeta.tarjeta--gris.p-4(titulo="Fase 2: Definir")
+        .row
+          .col-lg-3
+            figure
+              img.h150(src='@/assets/curso/t2-ic-x4.svg', alt='icono decorativo')
+          .col-lg-9
+            p.mb-3  Durante la fase de definición, el equipo de aprendices debe filtrar la información recopilada durante la fase de empatía. A partir de la pregunta esencial, los aprendices deben descomponerla en partes manejables, para que entiendan qué problema es el que se quiere resolver, esto se hace haciendo preguntas al instructor y a sus compañeros y quedar con lo que realmente aporta conocimiento y <b>que lleve al equipo a alcanzar su resolución de manera creativa e innovadora. </b>
+            p.mb-3.text-bold  Es importante resaltar que, a través de este proceso de preguntas y comentarios entre los aprendices, ellos entienden lo que deben hacer. 
+      
+      //- .tarjeta debe ir acompañado de una de una de estas clases => 
+      //- .color-primario, .color-secundario, .color-acento-contenido, .color-acento-botones
+      //- estas clases tambien tienen un modificador --borde
+      .tarjeta.tarjeta--gris.p-4(titulo="Fase 3: Idear")
+        .row
+          .col-lg-3
+            figure
+              img.h150(src='@/assets/curso/t2-ic-x3.svg', alt='icono decorativo')
+          .col-lg-9
+            p.mb-3  La fase de ideación tiene como objetivo la generación de un sinfín de opciones, se espera que cada grupo de aprendices divulguen sus conocimientos para luego, <b>entre todos, consolidar una solución colectiva. </b>
+            p.mb-3  En esta etapa también se evalúa qué tanto saben y qué tanto les hace falta para tener los conceptos claros los aprendices. Cabe mencionar que <b>el instructor orienta a los aprendices a diseñar prototipos que se ajusten a la necesidad o problema presentado.</b> 
+      
+      //- .tarjeta debe ir acompañado de una de una de estas clases => 
+      //- .color-primario, .color-secundario, .color-acento-contenido, .color-acento-botones
+      //- estas clases tambien tienen un modificador --borde
+      .tarjeta.tarjeta--gris.p-4(titulo="Fase 4: Prototipar")
+        .row
+          .col-lg-3
+            figure
+              img.h150(src='@/assets/curso/t2-ic-x2.svg', alt='icono decorativo')
+          .col-lg-9
+            p.mb-3  En la fase de prototipado los aprendices vuelven sobre <b>las ideas, los materiales y proceden a construir la solución diseñada en el prototipo.</b> El instructor les recuerda las restricciones que se tiene y la evaluación constante de sus ideas. Es esencial que los aprendices se dividan internamente por roles. <b>El rol le permite evaluar la forma en que van aprendiendo cada uno de los aprendices.</b>
+      
+      //- .tarjeta debe ir acompañado de una de una de estas clases => 
+      //- .color-primario, .color-secundario, .color-acento-contenido, .color-acento-botones
+      //- estas clases tambien tienen un modificador --borde
+      .tarjeta.tarjeta--gris.p-4(titulo="Fase 5 Testear")
+        .row
+          .col-lg-3
+            figure
+              img.h150(src='@/assets/curso/t2-ic-x1.svg', alt='icono decorativo')
+          .col-lg-9
+            p.mb-3  Durante la fase de testeo, se espera probar los prototipos. En este proceso es crucial identificar mejoras significativas, fallos a resolver, posibles carencias de las soluciones presentadas por los aprendices. 
+            p.mb-3  En el proceso de revisión encontrarán que hay cosas que no funcionan bien, y es el mejor momento para revisar los conceptos.   
+            p.mb-3  Como se ha mencionado, esta etapa es quizás una de las más importantes y a la que más tiempo se le debe asignar, porque es cuando los conceptos deben quedar correctamente entendidos. Al hacer las correcciones, los aprendices profundizan en su nivel de aprendizaje y, por lo tanto, su conocimiento será más duradero.
+
+
+
 
     Separador
     #t_2_4.titulo-segundo
       .h4 2.4 Elementos del pensamiento matemático
+    .row.mb-5
+      .col-lg-8
+        p.mb-3  <b>Las matemáticas ocupan un lugar muy importante en el desarrollo del pensamiento del ser humano,</b> desde los primeros años los niños se le impulsa a estudiarla, comprendiendo que todo a su alrededor la contiene: las compras de mercado, cocinar, conducir o utilizar cualquier dispositivo tecnológico; y para resolver problemas de la vida cotidiana, como planificar, gestionar, evaluar, entre otros aspectos.
+        p.mb-3  <b>El pensamiento lógico matemático se desarrolla de manera secuencial,</b> esto significa que se comienza por la compresión básica y se finaliza con la abstracción que ayuda al desarrollo de competencias matemáticas necesarias para que los seres humanos se relacionan en sociedad, afrontar problemas y dificultades y a superar todo tipo de conflictos, <b>estas habilidades son potencializadas con la educación STEM y sus metodologías constructivas a partir de áreas de conocimiento científico, tecnológico y de ingeniería.</b>
+      .col-lg-4
+        figure.mb-4
+          img(src='@/assets/curso/t2-i12.png', alt='Niño frente al tablero escribiendo una formula matemática')
 
 
+
+
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        .cajon.color-secundario.p-4.mb-4
+          .row
+            .col-lg-2
+              figure.mb-4
+                img(src='@/assets/curso/t1-ic5.svg', alt='icono decorativo relacionado a contenido web')
+
+            .col-lg-10
+              p.mb-4 Ahora, con el propósito de identificar el desarrollo de competencias STEM y sus implicaciones en la enseñanza y el aprendizaje, se sugiere identificar elementos del pensamiento matemático en la experiencia de los autores José Luis Lupiáñez y Juan Francisco Ruiz Hidalgo.
+              .row
+                .col-lg-8
+                  a.anexo.mb-4.mb-lg-0(href="https://en.wikipedia.org/wiki/Main_Page" target="_blank")
+                    .anexo__icono
+                      img(src="@/assets/template/icono-link.svg")
+                    .anexo__texto
+                      p <b>Enlace web.</b>  Diseño de tareas para el desarrollo de la competencia STEM: los problemas de modelización matemática.
+   
 
 
 </template>
@@ -204,4 +365,7 @@ export default {
 <style lang="sass" scoped>
 .h80{height: 80px}
 .tarjeta--gris{background-color: #D5D6E2}
+.h150{height: 150px}
+.h210{height: 210px}
+.tabs-b__tab{background-color: #EDFFF6}
 </style>
