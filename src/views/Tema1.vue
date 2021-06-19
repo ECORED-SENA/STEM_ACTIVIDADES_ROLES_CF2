77e8620eb8f1bfd398441a7a5a223e878a611a08
luisEfe80 <luismendez80@hotmail.com>
2021-06-18 20:09:45 -0400

    content adjustment

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -2,6 +2,132 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    .titulo-principal
+      .titulo-principal__numero
+        span  1
+      h1 La integración curricular: educación STEM
+    p.mb-5  Una buena forma de definir la educación STEM puede ser que una 
+      span.text-bold  persona instruida en STEM:
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        .bloque-texto-a.color-secundario.p-4.p-md-5.mb-5 
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-8
+              .bloque-texto-a__texto.p-4
+                p.mb-3 Es aquella que tiene el conocimiento suficiente y las habilidades para participar y progresar en la sociedad moderna, segura de sí misma, y con la capacidad para usar, manipular y evaluar las tecnologías presentes en la vida cotidiana, así como la capacidad de entender los principios científicos y los procesos tecnológicos necesarios para resolver problemas, desarrollar discusiones y tomar decisiones 
+                  span.text-bold  (Purzer, Strobel y Cardella, 2014).
+                p.mb-3  Con esta definición en mente, la misma resulta apropiada para entender que el reto de la educación STEM, en los entornos de aprendizaje tradicionales, 
+                  span.text-bold  es la integración curricular y esto tiene como efecto un enfoque holístico de las asignaturas.
+
+            .col-lg-4.mb-4.mb-lg-0
+              figure
+                img.h160(src='@/assets/curso/t1-ic1.svg', alt='icono decorativo')
+    .row.mb-5
+      .col-lg-8
+        p.mb-3  La integración curricular es, quizás, uno de los aspectos de innovación en educación que más llama la atención, aquí es necesario recordar que, de acuerdo con el Ministerio de Educación Nacional, en Colombia el currículo es el conjunto de criterios, planes de estudio, programas, metodologías, y procesos que contribuyen a la formación integral y a la construcción de la identidad cultural nacional, regional y local, incluyendo también los recursos humanos, académicos y físicos para poner en práctica las políticas y llevar a cabo el proyecto educativo institucional 
+          span.text-bold  (Congreso de la República de Colombia, 1994).
+        p.mb-3  Es evidente que en la enseñanza tradicional las asignaturas en forma separada no alcanzan a cumplir con el propósito de la educación en 
+          span.text-bold  las asignaturas STEM (Ciencia-Tecnología-Ingeniería-Matemática), 
+          | por lo que el mejor camino para llegar a este punto de desarrollo es elevar la tecnología al nivel de las ciencias y las matemáticas, incluir la ingeniería mediante el proceso de diseño y dar a las matemáticas el contexto apropiado a través de las prácticas.
+      .col-lg-4
+        figure
+          img(src='@/assets/curso/t1-i1.png', alt='Maestra de matemáticas explicando un tema en el tablero de color verde')
+    p.mb-5  La integración curricular en la educación en Colombia no es un asunto nuevo porque muchas instituciones educativas han comenzado a desarrollarla; sin embargo, es necesario aclarar los siguientes aspectos:
+    .row.mb-5
+      .col-lg-4
+        figure.mb-4
+          img(src='@/assets/curso/t1-ic2.svg', alt='icono decorativo relacionado a educación')
+        p.mb-3  La integración no significa abandonar los estándares curriculares. El currículo deberá responder con claridad a ellos, y a su vez 
+          span.text-bold  permitir que se pueda llevar a cabo alguno de los niveles de integración.
+      .col-lg-4
+        figure.mb-4
+          img(src='@/assets/curso/t1-ic3.svg', alt='icono decorativo relacionado a niñez y ciencias')
+        p.mb-3  El poder de la integración es uno de los aspectos fundamentales del éxito de la educación STEM, dado que 
+          span.text-bold   trae a los aprendices problemas de la vida real y permite que encuentren respuestas adecuadas y soluciones efectivas.
+      .col-lg-4
+        figure.mb-4
+          img(src='@/assets/curso/t1-ic4.svg', alt='icono decorativo relacionado a educación')
+        p.mb-3  
+          span.text-bold  Una educación STEM 
+          |  integrada no solo ayuda a los aprendices, sino también a los instructores para que estos vean todo el panorama de conocimientos que deben de 
+          span.text-bold  ser entregados a los aprendices y cómo se articulan entre ellos.
+    .row.justify-content-center
+      .col-lg-10
+        .cajon.color-secundario.p-4.mb-4
+          .row
+            .col-lg-2
+              figure.mb-4
+                img(src='@/assets/curso/t1-ic5.svg', alt='icono decorativo relacionado a contenido web')
+
+            .col-lg-10
+              p.mb-4 <b>Es importante recordar </b>que las prácticas de ciencias, ingeniería y tecnología fueron establecidas en el <b>Framework for K-12 Science Education - Practices Crosscutting Concepts, and Core Ideas,</b> el cual es un marco de referencia para la enseñanza de las ciencias desde preescolar hasta el grado 12 que produjo los <b>Nets Generations and Standards NGS</b> (Estándares de Ciencias para la Próxima Generación). Las prácticas de matemáticas son el aporte del <b> Common Core State Standards: Mathematical Practice. </b> Adicionalmente, los Estándares de tecnología de la ITEA contribuyen a las prácticas de tecnologías e ingeniería. <b>En esta guía se presenta la importancia de la integración curricular y la educación STEM. </b>
+              p.mb-5  Para más información, se sugiere revisar la página web del National Academies of Science, Engineering - Medicine (2012), 
+                span.text-bold  hacer clic en el icono y explorar el contenido.
+              .row
+                .col-lg-8
+                  a.anexo.mb-4.mb-lg-0(href="https://en.wikipedia.org/wiki/Main_Page" target="_blank")
+                    .anexo__icono
+                      img(src="@/assets/template/icono-link.svg")
+                    .anexo__texto
+                      p <b>Enlace web.</b> A FRAMEWORK FOR K-12 SCIENCE EDUCATION Practices, Crosscutting Concepts, and Core Ideas
+    #1_1.titulo-segundo
+      h2 1.1  El papel de la institución educativa para lograr la integración curricular
+    .row.mb-5
+      .col-lg-8
+        p.mb-3  El siglo XXI está presentando nuevas situaciones que hacen reflexionar sobre el futuro de la educación porque en la actualidad todos los aprendices e instructores están viviendo los cambios sociales, económicos, políticos, ambientales y tecnológicos. Todos estos retos se desplazan a la reflexión del sistema de educación donde los nuevos profesionales del siglo XXI estarán sometidos a nuevas condiciones que ni siquiera se pueden imaginar aún; esto exige que la educación se adapte a esas nuevas situaciones y brinde a los aprendices la capacidad de desenvolverse en todos los ámbitos, tanto personales como profesionales, 
+          span.text-bold  para contribuir a actuar positivamente frente a este nuevo escenario mundial.
+        p.mb-3  En todo establecimiento educativo el proyecto educativo institucional y su currículo tienen <b>un periodo de tiempo donde se pretende alcanzar unos objetivos para un tiempo específico,</b> al llegar el momento de su evaluación para ser actualizado, es posible que la visión y la misión de la institución cambien con el fin de alinearse con el contexto social y empresarial, y en este momento es necesario que los agentes estratégicos y tácticos decidan la adopción de la integración de las asignaturas y cómo transformar el plan de estudios desde el enfoque de la educación STEM. Al incluir la <b>educación STEM</b> en el plan de estudio es considerar que se requiere mayor tiempo para enseñarlos, <b>los instructores necesitan cambiar su forma de dictar la formación,</b> pues no solo van a profundizar más en los conceptos, sino que deberán hacerlo de forma integrada con sus colegas; por lo tanto, <b>se recomienda los siguientes aspectos al momento de las modificaciones:</b>
+
+      .col-lg-4
+        figure
+          img(src='@/assets/curso/t1-i2.png', alt='grupo de estudiantes sentados en una mesa analizando unos planos')
+    .row.mb-5.justify-content-center
+      .col-lg-2
+        figure.mb-4
+          img(src='@/assets/curso/t1-ic6.svg', alt='icono decorativo relacionado a lista de chequeo')
+     
+      .col-lg-8
+        ul.lista-ul
+          li 
+            i.fas.fa-check
+            | Establecer el listado completo de asignaturas por cada grado.
+          li 
+            i.fas.fa-check
+            | Determinar el requerimiento de planta profesional académica para desarrollar las asignaturas.
+          li 
+            i.fas.fa-check
+            | Revisar los estándares curriculares y los planes de estudio actuales para establecer los contenidos que se van a dictar.
+          li 
+            i.fas.fa-check
+            | La nueva forma de dictar las clases, así como el nuevo papel del instructor, requerirá de un nuevo diseño de unidades didácticas y actividades académicas.
+    .row.mb-3.justify-content-center
+      .col-lg-10
+        .cajon.color-secundario.p-4.mb-4
+          p Después de verificar las recomendaciones propuestas y considerar qué cambios van a causar mayor impacto en la institución educativa, 
+            span.text-bold  es importante establecer una estructura transitoria de implementación de la educación STEM.
+    
+    Separador
+
+    #1_2.titulo-segundo
+      h2 1.2  La educación STEM y el rol de los instructores en el contexto escolar
+    .row.mb-5
+      .col-lg-8
+        p.mb-3  En el presente siglo se presenta una gran demanda de nuevos instructores, con capacidad de liderazgo y habilidades de trabajo colaborativo, capaces de enfrentar nuevas situaciones y ser partícipes de las soluciones que requieren los problemas del mundo, preparados para aportar en la formación de los futuros ciudadanos. La capacitación y entrenamiento de los que están en servicio será la inversión más importante que deberán hacer los entes gubernamentales y las instituciones educativas para que sus competencias sean sólidas y fundamentadas; logrando así que  
+          span.text-bold   los instructores se identifiquen con su nuevo rol en la institución y el proceso de enseñanza y aprendizaje. 
+        p.mb-3  El interés por <b> las asignaturas STEM</b>  (ciencia-tecnología-ingeniería-matemáticas) a nivel mundial es creciente y esto ha dado como resultado que se esté midiendo el desempeño de los aprendices sobre sus habilidades de aprendizaje interdisciplinario. Es el caso de las pruebas del <b> Programme for International Student Assessment - PISA</b>  (Programa para la Evaluación Internacional de los Aprendices dirigido por la OCDE - Organización para la Cooperación y Desarrollo Económico), ha considerado hacer las mediciones de <b> la preparación de los aprendices en las asignaturas STEM.</b>
+
+      .col-lg-4
+        figure
+          img(src='@/assets/curso/t1-i3.png', alt='profesora charlando con sus estudiantes de primaria')
+    
+    p.mb-5  Esta iniciativa indica cómo se transforma la educación y cómo el instructor debe prepararse adecuadamente para contribuir a la nueva formación de los aprendices. No es un asunto de probabilidades, es una situación en la que se va a demandar a los instructores su habilidad para diseñar y desarrollar actividades interdisciplinarias, trabajando de la mano con sus colegas 
+      span.text-bold  (Purzer, Strobel y Cardella, 2014).
+    .row.justify-content-center
+      .col-lg-10
+        .cajon.color-secundario.p-4.mb-4
+          p Hasta aquí se ha tratado de argumentar la importancia de la integración curricular y el rol de los instructores en el marco de la implementación de la educación STEM y, por ello, surgen algunas inquietudes como: <b>¿cuál es su rol en la transformación de las prácticas de enseñanza-aprendizaje?,</b> esta pregunta moverá su reflexión y por ahora solo tiene una respuestas previas; pero con la intención de conseguir una respuesta con la cual poder identificarse, <b>se propone seguir con su experiencia de aprendizaje. Adelante.</b> 
+
+
 
     //- El componente "Muestras" tiene las aplicaciones de todos los componentes de la base
     //- se encuentra en src/components/Muestras.vue
@@ -25,4 +151,6 @@ export default {
 }
 </script>
 
-<style lang="sass" scoped></style>
+<style lang="sass" scoped>
+.h160{height: 160px}
+</style>
